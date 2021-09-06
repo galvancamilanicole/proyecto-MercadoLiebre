f2eb5f6f679a7f921a1a97ef0f195b0af0cd4203
galvancamilanicole <galvancamilanicole@gmail.com>
2021-09-06 17:05:43 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const publicPath = path.resolve(__dirname,"./public");
+app.use(express.static(publicPath));
+
+app.listen(3000,()=>console.log("Servidor corriendo en el puerto 3000"));
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve("./views/home.html"))
+})
+
+app.get("/registro",(req,res)=>{
+    res.sendFile(path.resolve("./views/register.html"))
+})
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.resolve("./views/login.html"))
+})
