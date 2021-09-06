46098ba28b341c32ae5d3767fad981db9f38c88f
galvancamilanicole <galvancamilanicole@gmail.com>
2021-09-06 17:32:42 -0300

    modifique app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const path = require("path");
 const publicPath = path.resolve(__dirname,"./public");
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>console.log("Servidor corriendo en el puerto 3000"));
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo en el puerto 3000")
+});
 
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve("./views/home.html"))
